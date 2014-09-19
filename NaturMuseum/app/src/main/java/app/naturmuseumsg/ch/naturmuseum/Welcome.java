e4727337765c56040858bf7fe0832e5c690400f9
Michi Gysel <michi@scythe.ch>
2014-09-19 11:29:39 +0200

    added travis file

--- NaturMuseum/app/src/main/java/app/naturmuseumsg/ch/naturmuseum/Welcome.java
+++ NaturMuseum/app/src/main/java/app/naturmuseumsg/ch/naturmuseum/Welcome.java
@@ -14,7 +14,6 @@ public class Welcome extends Activity {
         setContentView(R.layout.activity_welcome);
     }
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
