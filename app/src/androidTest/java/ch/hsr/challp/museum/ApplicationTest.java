c63355ec3a0833a0fdccfeeb9f3242177d71fc80
Michi Gysel <mgysel@hsr.ch>
2014-09-19 15:37:33 +0200

    changed package names and added a dummy test

---
+++ app/src/androidTest/java/ch/hsr/challp/museum/ApplicationTest.java
@@ -0,0 +1,21 @@
+package ch.hsr.challp.museum;
+
+import android.test.ActivityUnitTestCase;
+
+import junit.framework.Assert;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+
+public class ApplicationTest extends ActivityUnitTestCase {
+
+    public ApplicationTest() {
+        super(Welcome.class);
+    }
+
+    public void testFoo() {
+        Assert.assertEquals(1, 1);
+    }
+
+}
